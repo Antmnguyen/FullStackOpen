4f99149c05b714347127cb78df60d17750354efd
antgit <AntS@SWIFTEDGE>
2025-08-15 20:13:13 -0700

    Add files to part1a

---
+++ part1/part1a/introdemo/src/App.jsx
@@ -0,0 +1,22 @@
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+
+      <Hello name = 'HU TAO'/>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  )
+}
+
+export default App
