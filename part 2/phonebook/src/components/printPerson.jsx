1527546eec12e5e9fec44a0e612bb05e8f0f8ed2
antgit <AntS@SWIFTEDGE>
2025-08-27 00:07:12 -0700

    added space

--- part 2/phonebook/src/components/printPerson.jsx
+++ part 2/phonebook/src/components/printPerson.jsx
@@ -2,8 +2,7 @@ const printPerson = (person, handleDelete) => { //takes in a persons object
    //if (!person || !person.id) return null
    return (
     <p key={person.id}>
-      {person.name}: {person.number} 
-      <button onClick={() => handleDelete(person.id)}>delete</button>
+      {person.name}: {person.number}  <button onClick={() => handleDelete(person.id)}>delete</button>
     </p>
   )
 }
